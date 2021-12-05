b553137dbaeb7bea6bd5af0f7ec46f4274d10411
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-12-05 19:15:50 +0300

    Add files via upload

---
+++ 1consol/1consol/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _1consol
+{
+    internal class Program
+    {
+        static double f(double x)
+        {
+            try
+            {
+                if (x == -1) throw new Exception();
+                else return (3*x +4) / (Math.Sqrt(x*x + 2*x +1));
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("\n\t Начальное значение: a= ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("\n\t Конечное значение: b= ");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("\n\t шаг: h= ");
+                double h = double.Parse(Console.ReadLine());
+                for (double i = a; i <= b; i += h)
+                    try
+                    {
+                        Console.WriteLine($"y({i:f2})={f(i):f4}");
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"y({i})=error");
+                    }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n\n\t****Неверный формат ввода данных****\n\n");
+            }
+        }
+    }
+}
